7b310abfdf272c1ba76c43bea5a1bca866c100bc
jkenjayev <javohirwebdev@gmail.com>
2021-01-08 22:05:03 +0500

    get router with id

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("hello");
+app.get("/", (req, res) => {
+  res.send("hello");
 });
 
-app.post('/', (req, res) => {
-    res.send(['Clean Code', 'Soft skills', 'Python Crash course']);
-})
+app.post("/", (req, res) => {
+  res.send(["Clean Code", "Soft skills", "Python Crash course"]);
+});
 
+app.get("/:id", (req, res) => {
+  
+});
 app.listen(5000, () => {
-    console.log("Hey! I am listening the 5000th port");
-})
+  console.log("Hey! I am listening the 5000th port");
+});
