50a58469a1ccf96064d57a29b613c4b631cf9e2e
jkenjayev <69050649+jkenjayev@users.noreply.github.com>
2021-01-08 21:58:11 +0500

    http get and post routes

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("hello");
+});
+
+app.post('/', (req, res) => {
+    res.send(['Clean Code', 'Soft skills', 'Python Crash course']);
+})
+
+app.listen(5000, () => {
+    console.log("Hey! I am listening the 5000th port");
+})
